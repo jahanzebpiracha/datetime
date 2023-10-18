d9d56e9c73ee58ee3de3996a66dc95bb0b94410c
jahanzebpiracha <65681640+jahanzebpiracha@users.noreply.github.com>
2023-10-18 12:40:44 -0700

    Create datetime.py
    pthyon file

---
+++ datetime.py
@@ -0,0 +1,14 @@
+import datetime
+
+# Get the current date
+current_date = datetime.datetime.now()
+
+# Extract day, month, and year
+current_day = current_date.day
+current_month = current_date.month
+current_year = current_date.year
+
+# Print the results
+print(f"Current Day: {current_day}")
+print(f"Current Month: {current_month}")
+print(f"Current Year: {current_year}")
