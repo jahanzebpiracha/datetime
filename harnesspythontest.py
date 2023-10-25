d2909aa3476e2b0ae0b5820741ee3c0dc76c2829
jahanzebpiracha <65681640+jahanzebpiracha@users.noreply.github.com>
2023-10-24 18:37:38 -0700

    Update harnesspythontest.py

--- harnesspythontest.py
+++ harnesspythontest.py
@@ -7,11 +7,10 @@ current_date = datetime.datetime.now()
 current_day = current_date.day
 current_month = current_date.month
 current_year = current_date.year
-current_time = current_datetime.strftime("%H:%M:%S")
+
 
 
 # Print the results
 print(f"Current Day: {current_day}")
 print(f"Current Month: {current_month}")
 print(f"Current Year: {current_year}")
-print(f"Current Time: {current_time}")
