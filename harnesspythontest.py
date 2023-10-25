cdc7a4d783c67afecb39817419d3b4c008568824
jahanzebpiracha <65681640+jahanzebpiracha@users.noreply.github.com>
2023-10-24 18:22:08 -0700

    Update harnesspythontest.py

--- harnesspythontest.py
+++ harnesspythontest.py
@@ -1,21 +1,14 @@
-import random
 import datetime
-import json
 
-# Generate a random number between 1 and 5
-random_number = random.randint(1, 6)
+# Get the current date
+current_date = datetime.datetime.now()
 
-# Get the current hour
-current_hour = datetime.datetime.now().hour
+# Extract day, month, and year
+current_day = current_date.day
+current_month = current_date.month
+current_year = current_date.year
 
-# Calculate the sum
-result = random_number + current_hour
-
-# Create a list containing the random number, current hour, and their sum
-data_list = [random_number, current_hour, result]
-
-# Convert the list to a JSON object
-json_data = json.dumps(data_list)
-
-# Print the JSON object
-print(json_data)
+# Print the results
+print(f"Current Day: {current_day}")
+print(f"Current Month: {current_month}")
+print(f"Current Year: {current_year}")
