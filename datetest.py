1829d3d6de4d4df99ddc4ee4cfec7687c0d9d65e
jahanzebpiracha <65681640+jahanzebpiracha@users.noreply.github.com>
2023-11-12 20:43:13 -0500

    Update datetest.py

--- datetest.py
+++ datetest.py
@@ -1,23 +1,10 @@
-import unittest
-from harnesspythontest import get_current_year, is_leap_year
+import datetime
 
-class Testharnesspythontest(unittest.TestCase):
+def get_current_year():
+    return datetime.datetime.now().year
 
-    def test_get_current_year(self):
-        current_year = get_current_year()
-        self.assertIsInstance(current_year, int)
-        self.assertGreaterEqual(current_year, 2000)  # Adjust the starting year as needed
+def get_current_month():
+    return datetime.datetime.now().month
 
-    def test_is_leap_year(self):
-        # Test with known leap years
-        self.assertTrue(is_leap_year(2000))
-        self.assertTrue(is_leap_year(2020))
-        self.assertTrue(is_leap_year(2400))
-
-        # Test with known non-leap years
-        self.assertFalse(is_leap_year(1900))
-        self.assertFalse(is_leap_year(2100))
-        self.assertFalse(is_leap_year(2200))
-
-if __name__ == '__main__':
-    unittest.main()
+def get_current_day():
+    return datetime.datetime.now().day
